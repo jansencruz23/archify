b84e90e0d8187644dc4bd3a9a35b398287cce472
Jamaica Salem <jamaica.esalem@gmail.com>
2024-10-29 18:01:58 +0800

    :sparkles: Create reusable MyNavbar component for bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:archify/firebase_options.dart';
 import 'package:archify/pages/home_page.dart';
+import 'package:archify/pages/login_page.dart';
+import 'package:archify/pages/register_page.dart';
 import 'package:archify/pages/setup_page.dart';
 import 'package:archify/pages/setup_pages/setup_intro_page.dart';
 import 'package:archify/services/auth/auth_gate.dart';
@@ -35,7 +37,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightMode,
       initialRoute: '/',
-      routes: {'/': (context) => const AuthGate()},
+      routes: {'/': (context) => RegisterPage(onTap: () {},)},
     );
   }
 }
