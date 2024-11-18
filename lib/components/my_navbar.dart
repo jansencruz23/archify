b2676c3848fd908f3fd4218338b4a5965362c0d9
Jamaica Salem <jamaica.esalem@gmail.com>
2024-11-18 14:22:33 +0800

    :dizzy: Add animation on the plus button

--- lib/components/my_navbar.dart
+++ lib/components/my_navbar.dart
@@ -1,148 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  int selectedIndex = 0;
-  bool showVerticalBar = false;
-  late AnimationController _animationController;
-  late Animation<Offset> _slideAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animationController = AnimationController(
-      duration: Duration(milliseconds: 1000),
-      vsync: this,
-    );
-    _slideAnimation = Tween<Offset>(
-      begin: Offset(0, 1),
-      end: Offset(0, 0),
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      if (index == 2) {
-        if (showVerticalBar) {
-          _animationController.reverse();
-        } else {
-          _animationController.duration = Duration(milliseconds: 500);
-          _animationController.forward();
-        }
-        showVerticalBar = !showVerticalBar;
-      } else {
-        if (showVerticalBar) {
-          _animationController.reverse();
-          showVerticalBar = false;
-        }
-      }
-      selectedIndex = index;
-    });
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Navbar Example'),
-      ),
-      body: Stack(
-        children: [
-          Center(
-            child: Text('Content here'),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: MyNavbar(
-              selectedIndex: selectedIndex,
-              onItemTapped: _onItemTapped,
-            ),
-          ),
-          if (showVerticalBar)
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: SlideTransition(
-                position: _slideAnimation,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 500),
-                  height: MediaQuery.of(context).size.height * 0.5 + 80,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: IconButton(
-                          icon: Icon(Icons.keyboard_arrow_down, size: 30),
-                          onPressed: () {
-                            setState(() {
-                              _animationController.reverse();
-                              showVerticalBar = false;
-                            });
-                          },
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: 5,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              leading: Icon(Icons.circle, color: Colors.blue),
-                              title: Text('Item ${index + 1}'),
-                              onTap: () {},
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-}
-
 class MyNavbar extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onItemTapped;
+  final bool showVerticalBar;
+  final bool isRotated;
+  final Function toggleRotation;
 
   const MyNavbar({
     Key? key,
     required this.selectedIndex,
     required this.onItemTapped,
+    required this.showVerticalBar,
+    required this.isRotated,
+    required this.toggleRotation,
   }) : super(key: key);
 
   static const double navIconSize = 30.0;
@@ -157,31 +28,29 @@ class MyNavbar extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            _buildNavIcon(Icons.home, 0),
-            _buildNavIcon(Icons.favorite, 1),
+            _buildNavIcon('lib/assets/images/home_icon.png', 0),
+            _buildNavIcon('lib/assets/images/like_icon.png', 1),
             _buildElevatedNavIcon(),
-            _buildNavIcon(Icons.person, 3),
-            _buildNavIcon(Icons.settings, 4),
+            _buildNavIcon('lib/assets/images/user_icon.png', 3),
+            _buildNavIcon('lib/assets/images/setting_icon.png', 4),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildNavIcon(IconData icon, int index) {
-    bool isSelected = selectedIndex == index;
-
+  Widget _buildNavIcon(String iconPath, int index) {
     return GestureDetector(
       onTap: () => onItemTapped(index),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(
-            icon,
-            size: navIconSize,
-            color: isSelected ? Color(0xFFFF6F61) : Colors.black,
+          Image.asset(
+            iconPath,
+            height: navIconSize,
+            width: navIconSize,
           ),
-          if (isSelected)
+          if (selectedIndex == index)
             Container(
               margin: const EdgeInsets.only(top: 4),
               height: 8,
@@ -198,7 +67,10 @@ class MyNavbar extends StatelessWidget {
 
   Widget _buildElevatedNavIcon() {
     return GestureDetector(
-      onTap: () => onItemTapped(2),
+      onTap: () {
+        toggleRotation();
+        onItemTapped(2);
+      },
       child: Transform.translate(
         offset: const Offset(0, -15),
         child: Stack(
@@ -227,23 +99,152 @@ class MyNavbar extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              height: 30,
-              width: 30,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.add,
-                  color: Color(0xFFD2691E),
-                  size: 28,
+            AnimatedRotation(
+              turns: isRotated ? 0.5 : 0,
+              duration: Duration(milliseconds: 300),
+              child: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: Color(0xFFD2691E),
+                    size: 28,
+                  ),
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  int _selectedIndex = 0;
+  bool _showVerticalBar = false;
+  bool _isRotated = false;
+
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, 1),
+      end: Offset(0, 0),
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      if (index == 2) {
+        if (_showVerticalBar) {
+          _animationController.reverse();
+        } else {
+          _animationController.forward();
+        }
+        _showVerticalBar = !_showVerticalBar;
+      } else {
+        if (_showVerticalBar) {
+          _animationController.reverse();
+          _showVerticalBar = false;
+        }
+      }
+      _selectedIndex = index;
+    });
+  }
+
+  void _toggleRotation() {
+    setState(() {
+      _isRotated = !_isRotated;
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: MyNavbar(
+              selectedIndex: _selectedIndex,
+              onItemTapped: _onItemTapped,
+              showVerticalBar: _showVerticalBar,
+              isRotated: _isRotated,
+              toggleRotation: _toggleRotation,
+            ),
+          ),
+          if (_showVerticalBar)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: SlideTransition(
+                position: _slideAnimation,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  height: MediaQuery.of(context).size.height * 0.5 + 80,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: Icon(Icons.keyboard_arrow_down, size: 30),
+                          onPressed: () {
+                            setState(() {
+                              _animationController.reverse();
+                              _showVerticalBar = false;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: 5,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: Icon(Icons.circle, color: Colors.blue),
+                              title: Text('Item ${index + 1}'),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
