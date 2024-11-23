1c14ec72f9decd6cdd9778a3c6e011a97c0b026e
Jansen Cruz <jansencruz12345@gmail.com>
2024-11-23 10:49:39 +0800

    🚧: Update main.dart index page

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightMode,
       initialRoute: '/',
-      routes: {'/': (context) => RegisterPage(onTap: () {})},
+      routes: {'/': (context) => AuthGate()},
     );
   }
 }
