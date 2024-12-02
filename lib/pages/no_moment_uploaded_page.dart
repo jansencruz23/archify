c027cdb21cee0725391377539e98997c691812d4
Jamaica Salem <jamaica.esalem@gmail.com>
2024-12-02 21:55:10 +0800

    :lipstick: Add navbar component to no moment uploaded page

--- lib/pages/no_moment_uploaded_page.dart
+++ lib/pages/no_moment_uploaded_page.dart
@@ -1,5 +1,7 @@
-import 'package:archify/components/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:archify/components/my_button.dart';
+import 'package:archify/components/my_navbar.dart';
+import 'package:archify/pages/day_settings_page.dart';
 
 class NoMomentUploadedPage extends StatefulWidget {
   final void Function() imageUploadClicked;
@@ -9,74 +11,274 @@ class NoMomentUploadedPage extends StatefulWidget {
   State<NoMomentUploadedPage> createState() => _NoMomentUploadedPageState();
 }
 
-//For Responsiveness ng appbar text
-double _getClampedFontSize(BuildContext context, double scale) {
-  double calculatedFontSize = MediaQuery.of(context).size.width * scale;
-  return calculatedFontSize.clamp(12.0, 24.0); // Set min and max font size
-}
+class _NoMomentUploadedPageState extends State<NoMomentUploadedPage> with TickerProviderStateMixin {
+  int _selectedIndex = 1;
+  bool _showVerticalBar = false;
+  bool _isRotated = false;
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
 
-class _NoMomentUploadedPageState extends State<NoMomentUploadedPage> {
+  final List<Map<String, dynamic>> _menuItems = [
+    {'icon': Icons.wb_sunny, 'title': 'Enter a day code'},
+    {'icon': Icons.qr_code_scanner, 'title': 'Scan QR code'},
+    {'icon': Icons.add_circle_outline, 'title': 'Create a day'},
+    {'icon': Icons.settings, 'title': 'Settings'},
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      if (index == 2) {
+        if (_showVerticalBar) {
+          _animationController.reverse();
+        } else {
+          _animationController.forward();
+        }
+        _showVerticalBar = !_showVerticalBar;
+      } else {
+        if (_showVerticalBar) {
+          _animationController.reverse();
+          _showVerticalBar = false;
+        }
+      }
+      _selectedIndex = index;
+    });
+  }
+
+  void _toggleRotation() {
+    setState(() {
+      _isRotated = !_isRotated;
+    });
+  }
+
+  void _showEnterDayCodeDialog(BuildContext context) {
+    TextEditingController _codeController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color(0xFFFF6F61),
+          title: const Text(
+            'Enter Day Code',
+            style: TextStyle(fontFamily: 'Sora', color: Colors.white),
+          ),
+          content: TextField(
+            controller: _codeController,
+            cursorColor: Colors.white,
+            decoration: const InputDecoration(
+              hintText: 'Enter your code',
+              hintStyle: TextStyle(fontFamily: 'Sora', color: Colors.white70),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            style: const TextStyle(fontFamily: 'Sora', color: Colors.white),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(fontFamily: 'Sora', color: Colors.white),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                String enteredCode = _codeController.text;
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Code Entered: $enteredCode',
+                      style: const TextStyle(fontFamily: 'Sora'),
+                    ),
+                  ),
+                );
+              },
+              child: const Text(
+                'Enter',
+                style: TextStyle(fontFamily: 'Sora', color: Colors.white),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: const Offset(0, 0),
+    ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  double _getClampedFontSize(BuildContext context, double scale) {
+    double calculatedFontSize = MediaQuery.of(context).size.width * scale;
+    return calculatedFontSize.clamp(12.0, 24.0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(preferredSize: Size.fromHeight(70), child: AppBar(
-        titleSpacing: 0,
-        leadingWidth: 600,
-        leading: SizedBox(
-          height: double.infinity,
-          child: Padding(padding: const EdgeInsets.symmetric(horizontal: 24.0),child:
-          Stack(
-            children: [
-              Text('Let’s keep the moment,', style: TextStyle(
-                fontSize: _getClampedFontSize(context, 0.01),
-                fontFamily: 'Sora',
-                color: Theme.of(context)
-                    .colorScheme
-                    .inversePrimary,
-              ),),
-              Positioned(
-                  bottom: 10,
-                  left: 0,
-                  child:
-
-                  Text('Pick the best shot!', style: TextStyle(
-                    fontSize: _getClampedFontSize(context, 0.06),
-                    fontFamily: 'Sora',
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .inversePrimary,
-                  ),))
-            ],
-          ),),
-        ),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1),
-          child: Divider(
-            height: 2,
-            color: Theme.of(context).colorScheme.outline,
-          ),
-        ),
-      )),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(36.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('You haven\'t uploaded a moment yet!' ,textAlign: TextAlign.center, style: TextStyle(
-              fontSize: _getClampedFontSize(context, 0.05),
-          fontFamily: 'Sora',
-          color: Theme.of(context)
-              .colorScheme
-              .inversePrimary,
-        ),),
-              MyButton(
-                text: 'Upload your masterpiece',
-                onTap: widget.imageUploadClicked,
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(70),
+          child: AppBar(
+            titleSpacing: 0,
+            leadingWidth: 600,
+            leading: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Stack(
+                children: [
+                  Text(
+                    'Let’s keep the moment,',
+                    style: TextStyle(
+                      fontSize: _getClampedFontSize(context, 0.03),
+                      fontFamily: 'Sora',
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -5,
+                    left: 0,
+                    child: Text(
+                      'Pick the best shot!',
+                      style: TextStyle(
+                        fontSize: _getClampedFontSize(context, 0.05),
+                        fontFamily: 'Sora',
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(1),
+              child: Divider(
+                height: 2,
+                color: Theme.of(context).colorScheme.outline,
+              ),
+            ),
           ),
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'You haven\'t uploaded a moment yet!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: _getClampedFontSize(context, 0.05),
+                        fontFamily: 'Sora',
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                    ),
+                    MyButton(
+                      text: 'Upload your masterpiece',
+                      onTap: widget.imageUploadClicked,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: MyNavbar(
+                selectedIndex: _selectedIndex,
+                onItemTapped: _onItemTapped,
+                showVerticalBar: _showVerticalBar,
+                isRotated: _isRotated,
+                toggleRotation: _toggleRotation,
+                showEnterDayCodeDialog: _showEnterDayCodeDialog,
+              ),
+            ),
+            if (_showVerticalBar)
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: SlideTransition(
+                  position: _slideAnimation,
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    height: (_menuItems.length * 50).toDouble() + 100,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFFF6F61),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            icon: const Icon(Icons.keyboard_arrow_down, size: 30, color: Colors.white),
+                            onPressed: () {
+                              setState(() {
+                                _animationController.reverse();
+                                _showVerticalBar = false;
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: _menuItems.length,
+                            itemBuilder: (context, index) {
+                              final item = _menuItems[index];
+                              return ListTile(
+                                leading: Icon(item['icon'], color: Colors.white),
+                                title: Text(item['title'], style: const TextStyle(fontFamily: 'Sora', color: Colors.white)),
+                                onTap: () {
+                                  if (item['title'] == 'Enter a day code') {
+                                    _showEnterDayCodeDialog(context);
+                                  } else if (item['title'] == 'Create a day') {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => DaySettingsPage()),
+                                    );
+                                  }
+                                },
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
