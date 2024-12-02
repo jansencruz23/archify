49ed7cf059faf4b23ab61fa2d90a905434d04c10
Jansen Cruz <jansencruz12345@gmail.com>
2024-12-02 19:09:46 +0800

    💄: Display favorites on profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -251,6 +251,33 @@ class _ProfilePageState extends State<ProfilePage>
                 ),
                 body: Stack(
                   children: [
+                    RefreshIndicator(
+                      onRefresh: _loadData,
+                      color: Theme.of(context).colorScheme.secondary,
+                      child: MasonryGridView.builder(
+                          gridDelegate:
+                              SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2),
+                          shrinkWrap: true,
+                          itemCount: favoriteDays.length, //sample
+                          itemBuilder: (context, index) {
+                            final imagePath =
+                                favoriteDays[index].imageUrl; //sample
+                            if (imagePath.isEmpty) return SizedBox.shrink();
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16.0),
+                                child: Image.network(
+                                  imagePath, //sample
+                                  width: double.infinity,
+                                  height: (index % 3 == 0) ? 180 : 230,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
                     Positioned(
                       bottom: 0,
                       left: 0,
