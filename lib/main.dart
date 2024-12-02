38a1812a5ec2573baf20130b7f4cb5cda8970a44
Jansen Cruz <jansencruz12345@gmail.com>
2024-12-02 15:28:57 +0800

    🔀: Merge with JSalem and resolve conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:provider/provider.dart';
 
 void main() async {
   // Firebase setup
+  Provider.debugCheckInvalidValueType = null;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
