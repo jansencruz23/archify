60613e46c64f7680d923eedaa5e355847894c077
Jamaica Salem <jamaica.esalem@gmail.com>
2024-12-06 22:33:47 +0800

    :construction: Implement navbar on settings page

--- lib/services/auth/auth_gate.dart
+++ lib/services/auth/auth_gate.dart
@@ -18,7 +18,7 @@ class AuthGate extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const EmptyDayPage();
+            return const SettingsPage();
           } else {
             return const LoginOrRegister();
           }
