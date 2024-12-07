d5ac551ff17a2462fa7796a6dfcd42e5d63759eb
Jamaica Salem <jamaica.esalem@gmail.com>
2024-12-07 23:04:15 +0800

    :lipstick: Update day space page design

--- lib/services/auth/auth_gate.dart
+++ lib/services/auth/auth_gate.dart
@@ -18,7 +18,7 @@ class AuthGate extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return DaySpacePage(dayCode: '');
+            return const HomePage();
           } else {
             return const LoginOrRegister();
           }
