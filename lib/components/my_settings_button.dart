e5bcdac4dfbcfd6ec22128b41e59064353407355
Aljunalei Alfonso <154396730+Junalei@users.noreply.github.com>
2024-12-07 23:30:34 +0800

    :lipstick: Disable rate us when rated in settings page

--- lib/components/my_settings_button.dart
+++ lib/components/my_settings_button.dart
@@ -4,12 +4,13 @@ class MySettingsButton extends StatefulWidget {
   final String text;
   final Widget icon;
   final void Function()? onTap;
+  final bool isEnabled;
 
   const MySettingsButton({
     super.key,
     required this.text,
     required this.icon,
-    this.onTap,
+    this.onTap, this.isEnabled = true,
   });
 
   @override
@@ -25,44 +26,65 @@ class _MySettingsButtonState extends State<MySettingsButton> {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: GestureDetector(
-        onTap: widget.onTap,
+        onTap: widget.isEnabled ? widget.onTap : null,
+        // Only allow tap if enabled
         child: GestureDetector(
           onTap: () {
-            setState(() {
-              _isClicked = !_isClicked;
-            });
-            if (widget.onTap != null) {
-              widget.onTap!();
-            }
-            Future.delayed(Duration(milliseconds: 300), () {
+            if (widget.isEnabled) { // Check if enabled before responding to tap
               setState(() {
-                _isClicked = false;
+                _isClicked = !_isClicked;
               });
-            });
+              if (widget.onTap != null) {
+                widget.onTap!();
+              }
+              Future.delayed(Duration(milliseconds: 300), () {
+                setState(() {
+                  _isClicked = false;
+                });
+              });
+            }
           },
           onLongPressStart: (_) {
-            Future.delayed(const Duration(milliseconds: 100), () {
-              setState(() {
-                _isLongPressed = true;
+            if (widget
+                .isEnabled) { // Check if enabled before handling long press
+              Future.delayed(const Duration(milliseconds: 100), () {
+                setState(() {
+                  _isLongPressed = true;
+                });
               });
-            });
+            }
           },
-
           onLongPressEnd: (_) {
-            setState(() {
-              _isLongPressed = false;
-            });
+            if (widget
+                .isEnabled) { // Check if enabled before handling long press end
+              setState(() {
+                _isLongPressed = false;
+              });
+            }
           },
           child: AnimatedContainer(
             duration: Duration(microseconds: 100),
             height: 50,
             decoration: BoxDecoration(
-              color:
-              _isLongPressed
-                  ? Theme.of(context).colorScheme.secondaryContainer
-              : _isClicked
-                  ? Theme.of(context).colorScheme.secondaryContainer
-                  : Theme.of(context).colorScheme.surface,
+              color: widget.isEnabled
+                  ? (_isLongPressed
+                  ? Theme
+                  .of(context)
+                  .colorScheme
+                  .secondaryContainer
+                  : _isClicked
+                  ? Theme
+                  .of(context)
+                  .colorScheme
+                  .secondaryContainer
+                  : Theme
+                  .of(context)
+                  .colorScheme
+                  .surface)
+                  : Theme
+                  .of(context)
+                  .colorScheme
+                  .surface,
               borderRadius: BorderRadius.circular(15),
             ),
             child: Padding(
@@ -75,7 +97,15 @@ class _MySettingsButtonState extends State<MySettingsButton> {
                   Text(
                     widget.text,
                     style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
+                      color: widget.isEnabled
+                          ? Theme
+                          .of(context)
+                          .colorScheme
+                          .inversePrimary
+                          : Theme
+                          .of(context)
+                          .colorScheme
+                          .onSurfaceVariant,
                       fontFamily: 'Sora',
                       fontSize: 18,
                     ),
