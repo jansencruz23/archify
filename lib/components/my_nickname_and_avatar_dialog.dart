946b21a69c3ae97a639b1a237d7e846333082b6b
Aljunalei Alfonso <154396730+Junalei@users.noreply.github.com>
2024-12-09 19:10:59 +0800

    :lipstick: Reponsive button font on nickname and avatar

--- lib/components/my_nickname_and_avatar_dialog.dart
+++ lib/components/my_nickname_and_avatar_dialog.dart
@@ -49,6 +49,11 @@ class _MyNicknameAndAvatarDialogState extends State<MyNicknameAndAvatarDialog> {
     super.dispose();
   }
 
+  //Font responsiveness
+  double _getClampedFontSize(BuildContext context, double scale) {
+    double calculatedFontSize = MediaQuery.of(context).size.width * scale;
+    return calculatedFontSize.clamp(12.0, 24.0); // Set min and max font size
+  }
   @override
   Widget build(BuildContext context) {
     final fillColor = Theme.of(context).colorScheme.tertiary;
@@ -297,7 +302,7 @@ class _MyNicknameAndAvatarDialogState extends State<MyNicknameAndAvatarDialog> {
                       'Confirm Selection',
                       style: TextStyle(
                         fontFamily: 'Sora',
-                        fontSize: 18,
+                        fontSize: _getClampedFontSize(context, 0.04),
                         fontWeight: FontWeight.bold,
                       ),
                     ),
